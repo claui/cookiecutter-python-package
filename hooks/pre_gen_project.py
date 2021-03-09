78a491b372f1fcf7957d5ab542c199f2c16bf433
Claudia Pellegrino <Claudia.Pellegrino-extern@deutschebahn.com>
2021-03-09 20:42:56 +0100

    Check whether pipenv is installed

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,8 @@
 import sys
 
-assert(sys.version_info) >= (3, 5), \
-    'This Cookiecutter template requires Python 3.5 or higher.'
+if sys.version_info < (3, 5):
+    sys.exit(
+        """
+This Cookiecutter template requires Python 3.5 or higher.
+"""
+    )
