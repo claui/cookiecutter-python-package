964ad638d4ccc086996f3afcf4fa72f596313b8a
Claudia <claui@users.noreply.github.com>
2021-08-04 23:47:39 +0200

    Make venv work under Windows too

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,20 @@
 import os
+import platform
 import shutil
 import subprocess
 
 os.unlink('.venv/.keep')
 shutil.rmtree('licenses')
+
+# Pick platform-dependent pylint shim
+with open('.vscode/settings.json', 'r+') as file:
+    content = file.read()
+    file.seek(0)
+    file.write(content.replace(
+        '.SHIM_EXT',
+        '.bat' if platform.system() == 'Windows' else ''
+    ))
+    file.truncate()
 
 {%- if cookiecutter.use_fire != "y" %}
 os.unlink('src/fire_workarounds.py')
@@ -12,9 +23,9 @@ os.unlink('src/fire_workarounds.py')
 {%- if cookiecutter.install_dependencies_now == "y" %}
 pipenv_environment = os.environ.copy()
 pipenv_environment.update({
-        'LANG': pipenv_environment.get('LANG', 'en_US.UTF-8'),
-        'PIPENV_IGNORE_VIRTUALENVS': '1',
-        'SYSTEM_VERSION_COMPAT': '1',
+    'LANG': pipenv_environment.get('LANG', 'en_US.UTF-8'),
+    'PIPENV_IGNORE_VIRTUALENVS': '1',
+    'SYSTEM_VERSION_COMPAT': '1',
 })
 
 print('Running pipenv. This may take a while.')
