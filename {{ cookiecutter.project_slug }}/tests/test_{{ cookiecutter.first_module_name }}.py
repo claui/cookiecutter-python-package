76bdc607e0df97d37b9a60596965a5ba24c4ca27
Claudia <claui@users.noreply.github.com>
2021-08-08 19:15:22 +0200

    Fix test names according to README

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
@@ -3,12 +3,12 @@
 import {{ cookiecutter.project_slug }}
 
 
+def test_hello():
+    assert {{ cookiecutter.project_slug }}.hello() \
+        == 'Hello, world!'
+
+
 def test_hello_with_author_name():
     assert {{ cookiecutter.project_slug }} \
         .hello('{{ cookiecutter.author_full_name }}') \
         == 'Hello, {{ cookiecutter.author_full_name }}!'
-
-
-def test_hello_with_no_arguments():
-    assert {{ cookiecutter.project_slug }}.hello() \
-        == 'Hello, world!'
