11ae91f454e0aa0f18b7b16aa0b9233475808b71
Claudia <claui@users.noreply.github.com>
2021-08-08 19:13:01 +0200

    Simplify import in tests

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
@@ -1,15 +1,14 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, no-self-use, too-many-public-methods
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.first_module_name }}
+import {{ cookiecutter.project_slug }}
 
 
 def test_hello_with_author_name():
-    assert {{ cookiecutter.first_module_name }} \
+    assert {{ cookiecutter.project_slug }} \
         .hello('{{ cookiecutter.author_full_name }}') \
         == 'Hello, {{ cookiecutter.author_full_name }}!'
 
 
 def test_hello_with_no_arguments():
-    assert {{ cookiecutter.first_module_name }} \
-        .hello() \
+    assert {{ cookiecutter.project_slug }}.hello() \
         == 'Hello, world!'
