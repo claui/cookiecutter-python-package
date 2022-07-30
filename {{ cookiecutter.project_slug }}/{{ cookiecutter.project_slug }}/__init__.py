556ca0f4a1fe9381ebbe780e49af7e4723d25f96
Claudia Pellegrino <claui@users.noreply.github.com>
2022-07-30 16:13:33 +0200

    Remove unneeded import

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
@@ -1,6 +1,4 @@
 """Root package"""
 
-from {{ cookiecutter.project_slug }}.{{ cookiecutter.first_module_name }} import *
-
 # Make settings module visible for all subpackages
 __all__ = ['settings']
