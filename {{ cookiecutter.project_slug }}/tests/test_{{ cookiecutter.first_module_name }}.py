42ca1343725acbd9393a5db7795ce0dedc24d8e8
Claudia Pellegrino <claui@users.noreply.github.com>
2022-08-26 22:39:13 +0200

    Re-export symbol to top-level package

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
@@ -1,13 +1,12 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, no-self-use, too-many-public-methods
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.first_module_name }}
+from {{ cookiecutter.project_slug }} import hello
 
 
 def test_hello() -> None:
-    assert {{ cookiecutter.first_module_name }}.hello() \
-        == 'Hello, world!'
+    assert hello() == 'Hello, world!'
 
 
 def test_hello_with_author_name() -> None:
-    assert {{ cookiecutter.first_module_name }}.hello('{{ cookiecutter.author_full_name }}') \
+    assert hello('{{ cookiecutter.author_full_name }}') \
         == 'Hello, {{ cookiecutter.author_full_name }}!'
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
@@ -1,10 +1,19 @@
 """
 .. include:: ../README.md
+
+## API Documentation
 """
 
-# Make modules visible for all subpackages.
-# (This also exposes them to pdoc.)
+# Re-export these symbols
+# (This promotes them from {{ cookiecutter.project_slug }}.{{ cookiecutter.first_module_name }} to {{ cookiecutter.project_slug }})
+from {{ cookiecutter.project_slug }}.{{ cookiecutter.first_module_name }} import hello as hello
+
 __all__ = [
+    # Tell pdoc to pick up all re-exported symbols
+    'hello',
+
+    # Modules that every subpackage should see
+    # (This also exposes them to pdoc)
     '{{ cookiecutter.first_module_name }}',
     'settings',
 ]
