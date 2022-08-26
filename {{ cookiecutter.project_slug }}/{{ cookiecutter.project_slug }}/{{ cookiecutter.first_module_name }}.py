ecb8b73fa7a039cd10325910e024dd995a0405f6
Claudia Pellegrino <claui@users.noreply.github.com>
2022-08-26 22:39:49 +0200

    Add guard clause to boilerplate

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/{{ cookiecutter.first_module_name }}.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/{{ cookiecutter.first_module_name }}.py
@@ -8,7 +8,13 @@ def hello(name: str = 'world') -> str:
         whom to greet.
         If left empty, we greet the `world`.
 
+    :raises ValueError:
+        if `name` is empty.
+
     :return: a friendly Hello, addressed to `name`.
     """
+
+    if not name:
+        raise ValueError("Name cannot be empty.")
 
     return f'Hello, {name}!'
