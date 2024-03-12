9f4edd8248b8c9623fbb827e011465696de5cf2b
Claudia Pellegrino <claui@users.noreply.github.com>
2024-03-12 21:26:47 +0100

    Update tests

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.first_module_name }}.py
@@ -1,12 +1,19 @@
 # pylint: disable=magic-value-comparison, missing-function-docstring, missing-module-docstring, no-self-use, too-many-public-methods
 
-from {{ cookiecutter.project_slug }} import hello
+import pytest
+
+from {{ cookiecutter.project_slug }}.{{ cookiecutter.first_module_name }} import {{ cookiecutter.first_module_name.capitalize() }}
 
 
-def test_hello() -> None:
-    assert hello() == 'Hello, world!'
+@pytest.fixture(name='{{ cookiecutter.first_module_name }}')
+def fixture_{{ cookiecutter.first_module_name }}() -> {{ cookiecutter.first_module_name.capitalize() }}:
+    return {{ cookiecutter.first_module_name.capitalize() }}(foobar='baz')
 
 
-def test_hello_with_author_name() -> None:
-    assert hello('{{ cookiecutter.author_full_name }}') \
+def test_hello({{ cookiecutter.first_module_name }}: {{ cookiecutter.first_module_name.capitalize() }}) -> None:
+    assert {{ cookiecutter.first_module_name }}.hello() == 'Hello, world!'
+
+
+def test_hello_with_author_name({{ cookiecutter.first_module_name }}: {{ cookiecutter.first_module_name.capitalize() }}) -> None:
+    assert {{ cookiecutter.first_module_name }}.hello('{{ cookiecutter.author_full_name }}') \
         == 'Hello, {{ cookiecutter.author_full_name }}!'
